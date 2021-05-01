a5c2d1b265c3ac21d480e3870165f0f07a0cf92d
agravelot <antoine.gravelot@hotmail.fr>
2021-05-02 01:13:40 +0200

    fix: impossible to detect media requests

--- image_optimizer.go
+++ image_optimizer.go
@@ -49,9 +49,8 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	// Ignore non image requests
 
-	if IsImageRequest(req.Header.Get("accept")) {
-		// Return cahed result here.
-	}
+	// TODO Check if cacheable
+	// Return cached result here.
 
 	wrappedWriter := &responseWriter{
 		ResponseWriter: rw,
@@ -87,28 +86,4 @@ func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 // IsImageResponse Determine with Content-Type header if the response is an image.
 func IsImageResponse(contentType string) bool {
 	return strings.HasPrefix(contentType, "image/")
-}
-
-// IsImageRequest Determine with Accept header if the request ask for an image.
-func IsImageRequest(acceptHeader string) bool {
-	if len(acceptHeader) == 0 {
-		return false
-	}
-
-	accepts := strings.Split(acceptHeader, ",")
-
-	for _, value := range accepts {
-		println(value)
-		// If start with "text/html", return false
-		if value == "text/html" {
-			return false
-		}
-
-		// Avoid unseless loop
-		if strings.HasPrefix(value, "image/") {
-			return true
-		}
-	}
-
-	return true
 }
