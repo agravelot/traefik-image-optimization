9ca4f44067d4700276fd75b3518a5713c656c189
agravelot <antoine.gravelot@hotmail.fr>
2021-05-02 20:30:19 +0200

    fix: add missing redis config

--- config/config.go
+++ config/config.go
@@ -4,9 +4,14 @@ type ImaginaryConfig struct {
 	Url string `json:"url" yaml:"url" toml:"url"`
 }
 
+type RedisConfig struct {
+	Url string `json:"url" yaml:"url" toml:"url"`
+}
+
 // Config the plugin configuration.
 type Config struct {
 	Processor string          `json:"processor" yaml:"processor" toml:"processor"`
 	Imaginary ImaginaryConfig `json:"imaginary,omitempty" yaml:"imaginary,omitempty" toml:"imaginary,omitempty"`
 	Cache     string          `json:"cache" yaml:"cache" toml:"cache"`
+	Redis     RedisConfig     `json:"redis,omitempty" yaml:"redis,omitempty" toml:"redis,omitempty"`
 }
